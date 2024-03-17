abb43e0def9d56635031da3f7fd3d7d6f1a8158d
andriraymondo <andriraymondo@gmail.com>
2024-03-18 00:05:03 +0700

    add loading in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,28 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import { handleGithubLogin } from "@/lib/action";
 import LoginForm from "@/components/auth/login/page";
 import CarousellComponent from "@/components/common/carousell/carousell";
 import { useRouter } from "next/navigation";
 import styles from "./login.module.css";
-import React, { useEffect } from "react";
+import Loading from "@/app/loading";
 
 const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
   // Menggunakan useEffect untuk memastikan penggunaan useRouter hanya terjadi di sisi klien
   useEffect(() => {
-    // const router = useRouter();
+    setIsLoading(false);
   }, []); // Efek ini hanya berjalan sekali saat komponen terpasang
+
+  if (isLoading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
 
   return (
     <div className='container'>
